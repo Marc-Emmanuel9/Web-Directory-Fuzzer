8557eb8070d1533b163f4af285ec9a84d83de2c1
AT0M <63908456+Marc-Emmanuel9@users.noreply.github.com>
2023-07-31 17:09:27 +0200

    Progress Bar and wordlist
    Adding wordlist and a progress bar

--- directoryFuzz.py
+++ directoryFuzz.py
@@ -1,10 +1,34 @@
-import requests
-import sys
+import requests, sys, os
 
-# Variable
-wordlist = ["tools-dump", "secure-development-lifecycle", "blabla"]
+defaultPath = [os.path.dirname(os.path.realpath(__file__)) + "\wordlist\default.txt"]
 url = sys.argv[1]
 exclude_status = [404]
+
+def progress_bar(current, total, bar_length=20):
+    fraction = current / total
+
+    arrow = int(fraction * bar_length - 1) * '-' + '>'
+    padding = int(bar_length - len(arrow)) * ' '
+
+    ending = '\n' if current == total else '\r'
+
+    print(f'Progress: [{arrow}{padding}] {current}/{total}', end=ending)
+
+def getWordlist(path):
+    
+    file = open(path, "r")
+
+    wordlist = []
+
+    for word in file.readlines():
+        if '\n' in word:
+            wordlist.append(word[:-1])
+        else:
+            wordlist.append(word)
+
+    return wordlist
+
+wordlist = getWordlist(defaultPath[0])
 
 def getStatus(url, word):
     if url[-1] == "/":
@@ -14,7 +38,9 @@ def getStatus(url, word):
     
     return rq.status_code
 
+i = 0
 for word in wordlist:
+    i += 1
     result = getStatus(url, word)
 
     if int(result) not in exclude_status:
@@ -22,3 +48,5 @@ for word in wordlist:
             print(f'[*]  {result} : {word} -> {url}{word}')
         else:
             print(f'[*]  {result} : {word} -> {url}/{word}')
+    
+    progress_bar(i, len(wordlist), bar_length=20)
